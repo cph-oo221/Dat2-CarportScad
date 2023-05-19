1e9bc1281e1b40e748dc8a797d80101608ab7888
kotteletfisk <ostekugle@gmail.com>
2023-05-19 09:58:24 +0200

    Fixed config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import org.abstractica.javacsg.Geometry3D;
 import org.abstractica.javacsg.JavaCSG;
 import org.abstractica.javacsg.JavaCSGFactory;
 
-public class Test
+public class Main
 {
 	public static void main(String[] args)
 	{
