a4bdaa5d14885edf36b88f5fb5b33a71f1b7e5a6
kotteletfisk <ostekugle@gmail.com>
2023-05-19 12:15:27 +0200

    DB connectionpool

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import org.abstractica.javacsg.Geometry3D;
 import org.abstractica.javacsg.JavaCSG;
 import org.abstractica.javacsg.JavaCSGFactory;
+import persistence.ConnectionPool;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		ConnectionPool connectionPool = new ConnectionPool();
+
+
 		JavaCSG csg = JavaCSGFactory.createDefault();
 		Geometry3D cyl = csg.cylinder3D(10, 20, 32, true);
 		csg.view(cyl);
