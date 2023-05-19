bfe34da542db14b1a9ae34da8bbebb0c209872d5
Sebbedeb <zor.seb.0901@gmail.com>
2023-05-19 14:51:23 +0200

    Poles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,27 @@
+import Printables.Pole;
+import entities.OrderItem;
+import entities.Receipt;
+import entities.Wood;
+import exceptions.DatabaseException;
 import org.abstractica.javacsg.Geometry3D;
 import org.abstractica.javacsg.JavaCSG;
 import org.abstractica.javacsg.JavaCSGFactory;
 import persistence.ConnectionPool;
+import persistence.Facade;
+
+import javax.persistence.criteria.Order;
+import java.util.List;
 
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws DatabaseException
 	{
 		ConnectionPool connectionPool = new ConnectionPool();
 
-
 		JavaCSG csg = JavaCSGFactory.createDefault();
-		Geometry3D cyl = csg.cylinder3D(10, 20, 32, true);
-		csg.view(cyl);
+
+		Geometry3D poles = csg.union3D(Pole.print(20,74));
+
+		csg.view(poles);
 	}
 }
